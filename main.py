283f8c785ff605dbe09f54625705a4f580a548a3
ahmedsaif10 <66040378+ahmedsaif10@users.noreply.github.com>
2025-05-18 15:00:52 +0200

    Add files via upload
    front end for the application

---
+++ main.py
@@ -0,0 +1,75 @@
+import streamlit as st
+import tensorflow as tf
+import numpy as np
+
+
+#Tensorflow Model Prediction
+def model_prediction(test_image):
+    model = tf.keras.models.load_model("AI_chest_X-ray_diagnosis_model.keras")
+    image = tf.keras.preprocessing.image.load_img(test_image,target_size=(128,128))
+    input_arr = tf.keras.preprocessing.image.img_to_array(image)
+    input_arr = np.array([input_arr]) #convert single image to batch
+    predictions = model.predict(input_arr)
+    return np.argmax(predictions) #return index of max element
+
+#Sidebar
+st.sidebar.title("Dashboard")
+app_mode = st.sidebar.selectbox("Select Page",["Home","About","Disease Recognition"])
+
+#Main Page
+if(app_mode=="Home"):
+    st.header("CHEST X-RAY DIAGNOSIS SYSTEM")
+    image_path = "home_page.jpeg"
+    st.image(image_path,use_column_width=True)
+    st.markdown("""
+    Welcome to Our CHEST X-RAY DIAGNOSIS SYSTEM! 🔍
+    
+    Our mission is to help in Detect chest diseases efficiently. Upload an image of x-ray image, and our system will analyze it to detect any signs of diseases.
+
+    ### How It Works
+    1. **Upload Image:** Go to the **Disease detection** page and upload an the Image .
+    2. **Analysis:** Our system will process the image using advanced algorithms to identify potential diseases.
+    3. **Results:** View the results and recommendations for further action or Reports.
+    """)
+
+#About Project
+elif(app_mode=="About"):
+    st.header("About")
+    st.markdown("""
+                #### About Dataset
+                *******************************************************************************************************************************
+                This dataset consists of about 120k x-ray images of healthy , anomolyed and diseased  chest x-ray images  which is categorized into 14 different classes.
+                #### Content
+                1. train (80k images)
+                2. test (20k images)
+                3. validation (20k images)
+
+                """)
+
+#Prediction Page
+elif(app_mode=="Disease Recognition"):
+    st.header("Disease Recognition")
+    test_image = st.file_uploader("Choose an Image:")
+    if(st.button("Show Image")):
+        st.image(test_image,width=4,use_column_width=True)
+    #Predict button
+    if(st.button("Predict")):
+        st.snow()
+        st.write("Our Prediction")
+        result_index = model_prediction(test_image)
+        #Reading Labels
+        class_name = ['Apple___Apple_scab', 'Apple___Black_rot', 'Apple___Cedar_apple_rust', 'Apple___healthy',
+                    'Blueberry___healthy', 'Cherry_(including_sour)___Powdery_mildew', 
+                    'Cherry_(including_sour)___healthy', 'Corn_(maize)___Cercospora_leaf_spot Gray_leaf_spot', 
+                    'Corn_(maize)___Common_rust_', 'Corn_(maize)___Northern_Leaf_Blight', 'Corn_(maize)___healthy', 
+                    'Grape___Black_rot', 'Grape___Esca_(Black_Measles)', 'Grape___Leaf_blight_(Isariopsis_Leaf_Spot)', 
+                    'Grape___healthy', 'Orange___Haunglongbing_(Citrus_greening)', 'Peach___Bacterial_spot',
+                    'Peach___healthy', 'Pepper,_bell___Bacterial_spot', 'Pepper,_bell___healthy', 
+                    'Potato___Early_blight', 'Potato___Late_blight', 'Potato___healthy', 
+                    'Raspberry___healthy', 'Soybean___healthy', 'Squash___Powdery_mildew', 
+                    'Strawberry___Leaf_scorch', 'Strawberry___healthy', 'Tomato___Bacterial_spot', 
+                    'Tomato___Early_blight', 'Tomato___Late_blight', 'Tomato___Leaf_Mold', 
+                    'Tomato___Septoria_leaf_spot', 'Tomato___Spider_mites Two-spotted_spider_mite', 
+                    'Tomato___Target_Spot', 'Tomato___Tomato_Yellow_Leaf_Curl_Virus', 'Tomato___Tomato_mosaic_virus',
+                      'Tomato___healthy']
+        st.success("Model is Predicting it's a {}".format(class_name[result_index]))
